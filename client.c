7291f3e6ef251c0f1b005e416f087babfc7d5805
kvlksgjp1 <kvlksgjp1@naver.com>
2018-12-08 02:46:17 +0900

    serverUpdate

--- client.c
+++ client.c
@@ -8,21 +8,68 @@
 #include <sys/types.h>
 #include <netdb.h>
 #include <strings.h>
+#include <curses.h>
+#include <fcntl.h>
+#include <termio.h> 
 #include <pthread.h>
 #include <signal.h>
 
 #define CHATDATA 1024
+#define MAP_SIZE_ROW 10
+#define MAP_SIZE_COL 10
+#define UP 65
+#define DOWN 66
+#define RIGHT 67
+#define LEFT 68
+
+char	map[MAP_SIZE_ROW][MAP_SIZE_COL]=
+						{'2','2','2','2','2','2','2','2','2','2',
+						 '2',' ','G','A','M','E',' ',' ',' ','2',
+						 '2',' ',' ','S','T','A','R','T',' ','2',
+						 '2',' ',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2','2','2','2','2','2','2','2','2','2',};
 
 void *do_send_chat(void *);
 void *do_receive_chat(void *);
+void set_cr_noecho_mode(void);
+void enable_kbd_signals();
+void drawMap();
 
 pthread_t thread_1, thread_2;
 
 char escape[] = "exit";
 char nickname[20];
+int c_socket;
+int done=0;
+
+int connectServer(int argc, char *argv[]);
+void play();
 
 int main (int argc, char *argv[]) {
-	int c_socket;
+	
+	connectServer(argc, argv);
+	
+	play();
+	end();
+}
+
+void play()
+{
+
+	while( !done )
+	{		
+		pause();
+	}
+
+}
+
+int connectServer(int argc, char *argv[])
+{
 	struct sockaddr_in c_addr;
 	struct hostent *hp;
 	int len;
@@ -48,7 +95,7 @@ int main (int argc, char *argv[]) {
     
 	printf("Input Nickname : ");
 	scanf("%s", nickname);
-
+	getchar();
 	// 닉네임을 입력받고 설정한 주소를 통해 연결을 시도한다.
 	if (connect(c_socket, (struct sockaddr *)&c_addr, sizeof(c_addr)) == -1) {
 		printf("Can not connect\n");
@@ -59,6 +106,9 @@ int main (int argc, char *argv[]) {
 	pthread_create(&thread_1, NULL, do_send_chat, (void *) c_socket);
 	pthread_create(&thread_2, NULL, do_receive_chat, (void *) c_socket);
     
+	init();
+	//drawMap();
+
 	pthread_join(thread_1, NULL);
 	pthread_join(thread_2, NULL);
     
@@ -70,12 +120,12 @@ void *do_send_chat(void *arg) {
 	char buf[CHATDATA];
 	int n;
 	int c_socket = (int) arg;
-    
-	while(1) {
+
+	while(1) {/*
 		memset(buf, 0, sizeof(buf));
 		// buf에 값이 들어오면 이 값을 닉네임과 합쳐서 서버와 연결된 소켓으로 송신한다.
 		if ((n = read(0, buf, sizeof(buf))) > 0) {
-			sprintf(chatData, "[%s] %s", nickname, buf);
+			sprintf(chatData, "[%s]%s", nickname, buf);
 			write(c_socket, chatData, strlen(chatData));
 			
 			// 종료 문자열이 들어오면 스레드를 삭제한다.
@@ -83,20 +133,195 @@ void *do_send_chat(void *arg) {
 				pthread_kill(thread_2, SIGINT);
 				break;
 			}
-		}
+		}*/
 	}
+}
+
+
+void set_cr_noecho_mode(void)
+{
+	struct termios ttystate;
+	
+	tcgetattr( 0, &ttystate );
+	ttystate.c_lflag	&= ~ICANON;
+	ttystate.c_lflag	&= ~ECHO;
+	ttystate.c_cc[VMIN]	= 1;
+	tcsetattr( 0, TCSANOW, &ttystate);
+}
+
+void enable_kbd_signals()
+{
+	int  fd_flags;
+
+	fcntl(0, F_SETOWN, getpid());         
+	fd_flags = fcntl(0, F_GETFL);         
+	fcntl(0, F_SETFL, (fd_flags|O_ASYNC));
+}
+
+void on_input(int signum)
+{		
+	int 	c = getchar();		/* grab the char */
+	char chatData[CHATDATA];
+
+    if(c==27)
+    {
+        c=getchar();
+        c=getchar();
+    }
+	
+	//printf("%c caught\n",c);
+	if ( c == 'Q' || c == EOF )
+		done = 1;
+	else if ( c==' ' )
+	{
+		sprintf(chatData, "[%s]%s", nickname, "BOMB");
+		write(c_socket, chatData, strlen(chatData));
+	}
+	else if( c==LEFT )//LEFT
+	{
+		sprintf(chatData, "[%s]%s", nickname, "LEFT");
+		write(c_socket, chatData, strlen(chatData));
+	}
+	else if( c==RIGHT )//RIGHT
+	{
+		sprintf(chatData, "[%s]%s", nickname, "RIGHT");
+		write(c_socket, chatData, strlen(chatData));
+	}
+	else if( c==UP )//UP
+	{
+		sprintf(chatData, "[%s]%s", nickname, "UP");
+		write(c_socket, chatData, strlen(chatData));
+	}
+	else if( c==DOWN )//DOWN
+	{
+		sprintf(chatData, "[%s]%s", nickname, "DOWN");
+		write(c_socket, chatData, strlen(chatData));
+	}
+	else if( c=='q' )//DOWN
+	{
+		sprintf(chatData, "[%s]%s", nickname, "WALL");
+		write(c_socket, chatData, strlen(chatData));
+	}
+}
+
+void init()
+{
+	printf("init수행\n");
+	initscr();
+	set_cr_noecho_mode();
+	signal(SIGIO, on_input);     
+	enable_kbd_signals();     
+	start_color();
+	
+	drawMap();
 }
 
 void *do_receive_chat(void *arg) {
 	char chatData[CHATDATA];
 	int n;
 	int c_socket = (int) arg;
-    
+    int i,j;
+	int count=0;
+
 	while(1) {
 		memset(chatData, 0, sizeof(chatData));
 		// 읽어온 값이 있으면 값을 출력한다.
 		if ((n = read(c_socket, chatData, sizeof(chatData))) > 0) {
-			write(1, chatData, n);// 1은 표준 출력을 의미한다.
+			printf("서버로 부터 받은 데이터\n%s\n",chatData);
+			//write(1, chatData, n);// 1은 표준 출력을 의미한다.
+				count=0;
+				for(i=0;i<MAP_SIZE_ROW;i++)
+					for(j=0;j<MAP_SIZE_COL;j++)
+						map[i][j]=chatData[count++];
+
+				drawMap();
+				/*
+				for(i=0;i<MAP_SIZE_ROW;i++)
+					{
+						for(j=0;j<MAP_SIZE_COL;j++)
+						{
+							printf("%c",map[i][j]);
+						}
+					printf("\n");
+					}*/
+		
 		}
 	}
+}
+
+void end()
+{
+	endwin();
+}
+
+void drawMap()
+{
+	int i,j;
+    char cBlock=(char)0x2588;
+
+	clear();
+	init_pair(1,COLOR_RED,COLOR_BLACK);
+	init_pair(2,COLOR_GREEN,COLOR_BLACK);
+	init_pair(3,COLOR_BLUE,COLOR_BLACK);
+	init_pair(4,COLOR_CYAN,COLOR_BLACK);/* bomb pop color added */
+	init_pair(5,COLOR_YELLOW,COLOR_BLACK); //wall color
+
+	for(i=0;i<MAP_SIZE_ROW;i++)
+	for(j=0;j<MAP_SIZE_COL;j++)
+	{
+		move(i,j*2);		          /* get into position      */
+		if(map[i][j]==' ')
+		{
+			addch(' ');
+			move(i,j*2+1);
+			addch(' ');
+		}
+		else if(map[i][j]=='2')
+		{
+			attron(COLOR_PAIR(1));
+			addch(cBlock);
+			move(i,j*2+1);
+			addch(cBlock);
+			attroff(COLOR_PAIR(1));
+		}
+		else if(map[i][j]=='C')
+		{
+			attron(COLOR_PAIR(2));
+			addch(' '|A_REVERSE);
+			move(i,j*2+1);
+			addch(' '|A_REVERSE);
+			attron(COLOR_PAIR(2));
+		}
+		else if(map[i][j]=='M')
+		{
+			attron(COLOR_PAIR(3));
+			addch(' '|A_REVERSE);
+			move(i,j*2+1);
+			addch(' '|A_REVERSE);
+			attron(COLOR_PAIR(3));
+		}	
+		else if(map[i][j]=='B')
+				{
+					attron(COLOR_PAIR(4));
+					addch(' '|A_REVERSE);
+					move(i,j*2+1);
+					addch(' '|A_REVERSE);
+					attron(COLOR_PAIR(4));
+		} 
+		else if(map[i][j] == '1')
+				{
+					attron(COLOR_PAIR(5));
+					addch(' '|A_REVERSE);
+					move(i,j*2+1);
+					addch(' '|A_REVERSE);
+					attron(COLOR_PAIR(5));
+
+		}
+
+	}
+    //addch(' '|A_REVERSE);
+    //addch(' '|A_REVERSE);
+    //printw("0x2588: "); addch(cBlock); printw("\n");
+	move(50,50);
+	refresh();
 }
--- server.c
+++ server.c
@@ -1,18 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
+#include <netdb.h>
 #include <pthread.h> // gcc 옵션에 -lpthread 사용.
 
+#define MAP_SIZE_ROW 10
+#define MAP_SIZE_COL 10
+
+#define UP 65
+#define DOWN 66
+#define RIGHT 67
+#define LEFT 68
+
+typedef struct player_position{
+	int row;
+	int col;
+}player_position;
+
+int	delay = 20;	/* how long to wait	*/
+int	done  = 0;
+int power = 3;
+
+player_position position;
+
+char	map[MAP_SIZE_ROW][MAP_SIZE_COL]=
+						{'2','2','2','2','2','2','2','2','2','2',
+						 '2','C',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ','1',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ','1',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ','1',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2',' ',' ',' ',' ',' ',' ',' ',' ','2',
+						 '2','2','2','2','2','2','2','2','2','2',};
+
 void* do_chat(void *);
+void *boom();
+
+int set_ticker(int); 		
+void drawMap();
 
 pthread_t thread;
 pthread_mutex_t mutex;
 
 #define MAX_CLIENT	10
 #define CHATDATA	1024
+#define HOSTLEN 256
 #define INVALID_SOCK -1 // 클라이언트 소켓 배열의 초기 값.
 
 int list_c[MAX_CLIENT]; // 클라이언트들의 소켓 번호를 저장하기 위한 배열
@@ -24,8 +65,26 @@ char CODE200[] = "Sorry No More Connection\n";
 int popClient(int s);
 int pushClient(int c_socket);
 void *do_chat(void *arg);
+void on_alarm(int signum);
+int openServer(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
+	
+	init();
+	openServer(argc, argv);
+
+	return 0;
+}
+
+void init()
+{
+	position.row=1;
+	position.col=1;
+}
+
+int openServer(int argc, char *argv[])
+{
+
 	int c_socket, s_socket;
 	struct sockaddr_in s_addr, c_addr;
 	struct hostent *hp;
@@ -34,6 +93,8 @@ int main(int argc, char *argv[]) {
 	int i, j, n;
 	fd_set read_fds;
 	int res;
+	char hostname[HOSTLEN];
+	int client_count=1;
 
 	if (argc < 2) {
 		printf("usage: %s port_number\n", argv[0]);
@@ -48,13 +109,15 @@ int main(int argc, char *argv[]) {
 	s_socket = socket(PF_INET, SOCK_STREAM, 0); // TCP 소켓 생성
 	
 	// 연결 요청을 수신할 주소와 포트 설정.
-	memset(&s_addr, 0, sizeof(s_addr));
+	bzero( (void *)&s_addr, sizeof(s_addr));
 	gethostname(hostname,HOSTLEN);
 	hp=gethostbyname(hostname);
-	bcopy((void*)hp->h_addr, (void*)&saddr.sin_addr, hp->h_length);
+	bcopy((void*)hp->h_addr, (void*)&s_addr.sin_addr, hp->h_length);
 	//s_addr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY: IP주소
 	s_addr.sin_family = AF_INET;
 	s_addr.sin_port = htons(atoi(argv[1]));
+
+	printf("주소와 포트설정 완료\n");
 	
 	// 위에서 만든 주소를 소켓에 연결.
 	if (bind(s_socket, (struct sockaddr *)&s_addr, sizeof(s_addr)) == -1) {
@@ -62,12 +125,15 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
     
+	printf("바인딩 완료\n");
+
 	// 운영체제에 개통 요청. 이 시점부터 연결을 받아들인다.
 	if (listen(s_socket, MAX_CLIENT) == -1) {
 		printf("listen Fail\n");
 		return -1;
 	}
     
+	printf("연결 준비 완료\n");
 	// 클라이언트 소켓 배열의 초기 값 세팅.
 	for (i = 0; i < MAX_CLIENT; i++)
 		list_c[i] = INVALID_SOCK;
@@ -75,8 +141,11 @@ int main(int argc, char *argv[]) {
 	while(1) {
 		// 클라이언트로부터의 연결 요청 수신. 듣기 소켓과 연결 소켓.
 		len = sizeof(c_addr);
+
+        printf("새로운 accept 시도중...\n");
 		c_socket = accept(s_socket, (struct sockaddr *)&c_addr, &len);
-        
+        printf("%d번째 클라이언트와 연결\n",client_count++);
+
 		// 연결 소켓의 번호를 클라이언트 배열에 추가.
 		res = pushClient(c_socket);
 		if (res < 0) {
@@ -95,18 +164,80 @@ void *do_chat(void *arg) {
 	int c_socket = (int) arg;
 	char chatData[CHATDATA];
 	int i, n;
-	
+	char *ptr;
+	pthread_t thr;
+	int thr_id;
+
 	while(1) {
 		memset(chatData, 0, sizeof(chatData));
 		
 		// 소켓으로부터 읽어온 데이터가 있으면 전체 클라이언트에 메시지를 보낸다.
 		if((n = read(c_socket, chatData, sizeof(chatData))) > 0) {
-			for (i = 0; i < MAX_CLIENT; i++){
+			printf("받은 데이터 : %s\n",chatData);
+			/*for (i = 0; i < MAX_CLIENT; i++){
 				if (list_c[i] != INVALID_SOCK) {
 					write(list_c[i], chatData, n);
 				}
+			}*/
+
+			if(contain(chatData,"LEFT"))
+			{
+				if(map[position.row][position.col-1]!='2' && map[position.row][position.col-1]!='M' && map[position.row][position.col-1]!='1'){
+					if(map[position.row][position.col]!='M' && map[position.row][position.col]!='1'){
+						update(position.row,position.col,' ');
+					}
+					position.col-=1;
+					update(position.row,position.col,'C');
+				}
 			}
-            
+			else if(contain(chatData,"RIGHT"))
+			{
+				if(map[position.row][position.col+1]!='2' && map[position.row][position.col+1]!='M' && map[position.row][position.col+1]!='1'){
+					if(map[position.row][position.col]!='M' && map[position.row][position.col]!='1'){
+						update(position.row,position.col,' ');
+					}
+					position.col+=1;
+					update(position.row,position.col,'C');
+				}
+			}
+			else if(contain(chatData,"UP"))
+			{
+				if(map[position.row-1][position.col]!='2' && map[position.row-1][position.col]!='M' && map[position.row-1][position.col]!='1'){
+					if(map[position.row][position.col]!='M' && map[position.row][position.col]!='1'){
+						update(position.row,position.col,' ');
+					}
+					position.row-=1;
+					update(position.row,position.col,'C');
+				}
+			}
+			else if(contain(chatData,"DOWN"))
+			{
+				if(map[position.row+1][position.col]!='2' && map[position.row+1][position.col]!='M' && map[position.row+1][position.col]!='1'){
+					if(map[position.row][position.col]!='M' && map[position.row][position.col]!='1'){
+						update(position.row,position.col,' ');
+					}
+					position.row+=1;
+					update(position.row,position.col,'C');
+				}
+			}
+			else if(contain(chatData,"BOMB"))
+			{
+				 if(map[position.row][position.col]!='M'){
+					 thr_id=pthread_create(&thr, NULL, boom, NULL);
+				   if (thr_id < 0)
+			   	 {
+						perror("thread create error : ");
+						exit(0);
+					}
+				//pthread_join(thr,(void **)&status);
+				}
+			}
+			else if(contain(chatData,"WALL"))
+			{
+				 update(position.row,position.col,'1');
+			}
+
+            sendMap();
 			// 종료 문자열이 포함되어 있으면 해당 클라이언트를 배열에서 지운다.
 			if (strstr(chatData, escape) != NULL) {
 				popClient(c_socket);
@@ -114,6 +245,55 @@ void *do_chat(void *arg) {
 			}
 		}
 	}
+}
+
+void update(int row, int col, char value)
+{
+	map[row][col]=value;
+}
+
+int contain(char *s1, char *find)
+{	
+	char *ptr;
+
+	ptr=strstr(s1,find);
+	if(ptr!=NULL)
+		return 1;
+
+	return 0;
+}
+
+void startSend()
+{
+
+
+}
+
+void sendMap()
+{
+	int i,j;
+	char *mapdata= "server:mapdata:" ;
+	int count=0;
+	for (i = 0; i < MAX_CLIENT; i++){
+				if (list_c[i] != INVALID_SOCK) {
+					//write(list_c[i], mapdata, sizeof(100));
+					write(list_c[i], map, 100);
+				}
+			}
+	
+
+	for(i=0;i<MAP_SIZE_ROW;i++)
+		{for(j=0;j<MAP_SIZE_COL;j++)
+			{printf("%c",map[i][j]);}
+		printf("\n");}
+
+	printf("맵 전송 완료\n");
+}
+
+void on_alarm(int signum)
+{
+	signal(SIGALRM, on_alarm);	/* reset, just in case	*/
+	sendMap();
 }
 
 int pushClient(int c_socket) {
@@ -150,4 +330,116 @@ int popClient(int s) {
 		pthread_mutex_unlock(&mutex);
 	}
 	return 0;
+}
+
+void *boom() /*2018.11.30 KJY add*/
+{
+	player_position bomb = position;
+	update(position.row,position.col,'M');
+
+	sleep(3);
+
+	if(map[bomb.row][bomb.col]=='M')/* if it doesn't explode with earlier other thread*/
+		explosion(bomb.row,bomb.col);/* bomb at thread's original position */
+}
+
+void explosion(int row,int col){
+	int i=0;
+
+	update(row,col,'B');
+	for(i=1;i<=power;i++){
+		if(map[row][col+i]!='2')
+		{
+			if(map[row][col+i]=='M')
+				{explosion(row,col+i);
+				break;}	
+			update(row,col+i,'B');}
+		else
+			break;
+		}
+
+	for(i=1;i<=power;i++){
+		if(map[row][col-i]!='2')
+		{
+			if(map[row][col-i]=='M')
+				{explosion(row,col-i);
+				break;}	
+			update(row,col-i,'B');}
+		else
+			break;
+		}
+
+	for(i=1;i<=power;i++){
+		if(map[row+i][col]!='2')
+		{
+			if(map[row+i][col]=='M')
+				{explosion(row+i,col);
+				break;}	
+			update(row+i,col,'B');}
+		else
+			break;
+		}
+
+	for(i=1;i<=power;i++){
+		if(map[row-i][col]!='2')
+		{
+			if(map[row-i][col]=='M')
+				{explosion(row-i,col);
+				break;}	
+			update(row-i,col,'B');}
+		else
+			break;
+		}
+	sendMap();
+	usleep(100000);
+	
+	update(row,col,' ');	
+	for(i=1;i<=power;i++){
+		if(map[row][col+i]!='2')
+		{
+			update(row,col+i,' ');}
+		else
+			break;
+		}
+
+	for(i=1;i<=power;i++){
+		if(map[row][col-i]!='2')
+		{	
+			update(row,col-i,' ');}
+		else
+			break;
+		}
+
+	for(i=1;i<=power;i++){
+		if(map[row+i][col]!='2')
+		{
+			update(row+i,col,' ');}
+		else
+			break;
+		}
+
+	for(i=1;i<=power;i++){
+		if(map[row-i][col]!='2')
+		{
+			update(row-i,col,' ');}
+		else
+			break;
+		}
+	sendMap();
+}
+
+int set_ticker( int n_msecs )
+{
+	struct itimerval new_timeset;
+	long    n_sec, n_usecs;
+	
+	n_sec = n_msecs / 1000 ;			/* int part*/
+	n_usecs = ( n_msecs % 1000 ) * 1000L ;  	/* remainder*/
+
+	new_timeset.it_interval.tv_sec  = n_sec;        /* set reload       */
+	new_timeset.it_interval.tv_usec = n_usecs;      /* new ticker value */
+	new_timeset.it_value.tv_sec     = n_sec  ;      /* store this       */
+	new_timeset.it_value.tv_usec    = n_usecs ;     /* and this         */
+
+	return setitimer(ITIMER_REAL, &new_timeset, NULL);
 }
